d6832fed5cfb01abe618f9fe1976c428cedd70d5
Marcel Gruenauer <hanekomu@gmail.com>
2011-12-02 08:35:09 +1300

    Improved test for pl_file_ok() (thanks robinsmidsrod and mst)
    Note that this commit is a partial copy of a commit made by
    Marcel after his last release, but before handing over maintanership
    of Test::Compile.  I say partial because I had to reconstruct the
    intent of that commit from 2bf2144500aaa97f5e9780b5f6c6d017cbd37df9
    - which included these changes, plus some packaging changes.
    
    --
    Evan Giles

--- lib/Test/Compile.pm
+++ lib/Test/Compile.pm
@@ -71,10 +71,10 @@ sub pl_file_ok {
         $Test->diag("$file does not exist");
         return;
     }
-    my $out = `$^X -cw $file 2>&1`;
-    if ($?) {
+    system($^X, (map { "-I$_" } split ':', $ENV{PERL5LIB}), '-c', $file);
+    if (my $error = $?) {
         $Test->ok(0, 'Script does not compile');
-        $Test->diag($out);
+        $Test->diag($error);
         return;
     } else {
         $Test->ok(1, $name);
@@ -332,23 +332,6 @@ The order of the files returned is machine-dependent. If you want them
 sorted, you'll have to sort them yourself.
 
 =back
-
-=head1 BUGS AND LIMITATIONS
-
-No bugs have been reported.
-
-Please report any bugs or feature requests through the web interface at
-L<http://rt.cpan.org>.
-
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
-
-=head1 AVAILABILITY
-
-The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see L<http://search.cpan.org/dist/Test-Compile/>.
 
 =head1 AUTHORS
 
