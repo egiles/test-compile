53e27ddd34e2dc5cb5abb030eab713c293128b12
Sébastien Aperghis-Tramoni <sebastien@aperghis.net>
2013-01-22 15:20:58 +0100

    [CPAN-RT #82887] correctly handle library search paths

--- lib/Test/Compile/Internal.pm
+++ lib/Test/Compile/Internal.pm
@@ -158,10 +158,9 @@ sub pl_file_compiles {
     return $self->_run_closure(
         sub{
             if ( -f $file ) {
-                my @perl5lib = split(':', ($ENV{PERL5LIB}||""));
+                my @inc = ('blib/lib', @INC);
                 my $taint = $self->_is_in_taint_mode($file);
-                unshift @perl5lib, 'blib/lib';
-                system($^X, (map { "-I$_" } @perl5lib), "-c$taint", $file);
+                system($^X, (map { "-I$_" } @inc), "-c$taint", $file);
                 return ($? ? 0 : 1);
             }
         }
