951b2d79b5d7a4392e0d75a47d8b3e46b353d017
Evan Giles <egiles@cpan.org>
2019-06-26 13:31:12 +1200

    Update copyright year

--- lib/Test/Compile.pm
+++ lib/Test/Compile.pm
@@ -392,7 +392,7 @@ Evan Giles, C<< <egiles@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007-2015 by the authors.
+Copyright 2007-2019 by the authors.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
